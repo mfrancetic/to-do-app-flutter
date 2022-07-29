80a5d7e5dfd6166ff6647b91117cde9a303edace
Maja Francetic <francetic.maja@gmail.com>
2022-07-29 13:11:53 +0200

    Convert TaskData's tasks list to UnmodifiableListView, and modify just the private _tasks list

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_todoey/models/task.dart';
+import 'dart:collection';
 
 class TaskData with ChangeNotifier {
-  List<Task> tasks = [
+  final List<Task> _tasks = [
     Task(name: 'Buy milk'),
     Task(name: 'Buy eggs'),
     Task(name: 'Buy bread'),
   ];
 
+// cannot be modified
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
   int get taskCount {
-    return tasks.length;
+    return _tasks.length;
   }
 
   void addTask(String title) {
-    tasks.add(Task(name: title));
+    _tasks.add(Task(name: title));
     notifyListeners();
   }
 
   void toggleTaskDone(int index) {
-    tasks[index].toggleDone();
+    _tasks[index].toggleDone();
     notifyListeners();
   }
 }
